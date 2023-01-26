d771f6fee23f714170938e71d38accb35c9c64df
Ademir <ademir@debian.local>
2023-01-26 19:07:32 -0300

    finaly metodo insertItens

--- pg/Sale.ts
+++ pg/Sale.ts
@@ -49,24 +49,28 @@ export class Sale extends Pg{
              this._num_sale = id + 1;
              console.log(this._num_sale);
         
-             console.log("inserindo os itens vendidos")
+             console.log("inserindo os itens vendidos !!")
              for(let i= 0; itensales.length > i; i++){
              let sum_total_item:number = 0
              sum_total_item = itensales[i].amount * itensales[i].val_product;
              await client.query('insert into itens_sale("fk_sale", "fk_product", "amount_product", "val_product", "total_product") values ('+"'"+this._num_sale+"', '"+itensales[i].id_product+"', '"+itensales[i].amount+"', '"+itensales[i].val_product+"','"+sum_total_item+"');")
              }
-             console.log("Produtos inserido na tabela !")
              const result_itens = await client.query("SELECT * FROM itens_sale")
              let resultado = result_itens.rows
+             console.log("Produtos inserido na tabela !!")
              console.table(resultado)
 
-        
-             console.log("Lancando a Venda")
-        //     await client.query('insert into sales("fk_name_pers", "val_rec", "disc_sale", "total_sale") VALUES ('+"'"+this._name+"', '"+this._sub_total_sale+"', '"+this._disc_sale+"', '"+this._total_sale+"');")
-        //     //val_rec as sub_total_sale
-        //     const result_sale = await client.query("select * from products")
-        //    console.table(result_sale.rows)
-        //      console.log("Venda efetuada com sucesso")
+            console.log("Lancando a Venda")
+            const result_total_itens = await client.query("SELECT SUM(total_product) AS total FROM itens_sale where fk_sale = '"+this._num_sale+"'")
+            let sub_total_sale:number = 0
+            sub_total_sale = result_total_itens.rows[0].total
+            let total_sale:number = sub_total_sale -this._disc_sale
+
+             await client.query('insert into sales("val_rec", "disc_sale", "total_sale") VALUES ('+"'"+sub_total_sale+"', '"+this._disc_sale+"', '"+total_sale+"');")
+             //val_rec as sub_total_sale
+             const result_sale = await client.query("select * from sales")
+             console.table(result_sale.rows)
+             console.log("Venda efetuada com sucesso")
 
         }
         catch(ex){
--- pg/main.ts
+++ pg/main.ts
@@ -29,9 +29,9 @@ const product:Pg = new Product(3, "Caixa Som Mult", 49.90, 39.00, 1, 1)
 
 //const person:Pg = new Person(3, "Augusto Muller de Almeida", "99999999974","Rua Joao Sherer", 1)
 
-let itensales = [{id:1, name:1, id_product:1, amount:2, val_product:19.90, disc_sale:2.5},
-                 {id:2, name:2, id_product:2, amount:2, val_product:29.90, disc_sale:2.5},
-                 {id:3, name:3, id_product:3, amount:3, val_product:49.90, disc_sale:2.5}]
+let itensales = [{id:1, name:1, id_product:3, amount:4, val_product:23, disc_sale:12},
+                 {id:2, name:2, id_product:2, amount:4, val_product:25, disc_sale:8},
+                 {id:3, name:3, id_product:1, amount:4, val_product:45, disc_sale:7}]
 
 
 var sale:Pg = new Sale(itensales[0].id, itensales[0].name, itensales[0].id_product, itensales[0].amount, itensales[0].val_product, itensales[0].disc_sale)
