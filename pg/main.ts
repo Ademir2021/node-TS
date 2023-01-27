013de0f72cf02a97c6de44268fa067c23247bf59
Ademir <ademir@debian.local>
2023-01-27 10:46:12 -0300

    create  interfaces

--- pg/main.ts
+++ pg/main.ts
@@ -1,9 +1,11 @@
-import { Pg } from "./Pg"
-import { User } from './User'
+import { Pg } from "./Pg";
+import { User } from './User';
 import { Product } from './Product';
-import { Person } from './Person'
-import { Sale } from './Sale'
-let users:IUser[] = []
+import { Person } from './Person';
+import { Sale } from './Sale';
+let users:IUser[] = [];
+let sales:ISales;
+let itensales:IItensales[] = [];
 
 interface IUser{
     id:number;
@@ -12,27 +14,34 @@ interface IUser{
     password:string;
 };
 
-users = [
-    {id:1, name:'Ademir Souza de Almeida', username:'ademir@provider.com', password:'123abc'},
-    {id:2, name:'Maria Luiza de Almeida', username:'maria@provider.com', password:'123abc'},
-    {id:3, name:'Augusto Muller de Almeida', username:'augusto@provider.com', password:'123abc'},
-    {id:4, name:'Gustavo Souza de Almeida', username:'gustavo@provider.com', password:'123abc'},
-    {id:5, name:'Francieli Dalsente de Almeida', username:'francieli@provider.com', password:'123abc'}
-    ]
+interface IItensales{
+    id_product:number;
+    amount:number;
+    val_product:number;
+};
 
-const user:Pg = new User(users[0].id, users[0].name, users[0].username, users[0].password)
+interface ISales{
+    id:number;
+    name:string | number;
+    disc_sale:number;
+};
 
-const product:Pg = new Product(3, "Caixa Som Mult", 49.90, 39.00, 1, 1)
+users = [{id:1, name:'Ademir Souza de Almeida', username:'ademir@provider.com', password:'123abc'},
+         {id:2, name:'Maria Luiza de Almeida', username:'maria@provider.com', password:'123abc'}];
 
-const person:Pg = new Person(3, "Augusto Muller de Almeida", "99999999974","Rua Joao Sherer", 1)
+const user:Pg = new User(users[0].id, users[0].name, users[0].username, users[0].password);
 
-let itensales = [{id_product:1, amount:2, val_product:19.90},
+const product:Pg = new Product(3, "Caixa Som Mult", 49.90, 39.00, 1, 1);
+
+const person:Pg = new Person(3, "Augusto Muller de Almeida", "99999999974","Rua Joao Sherer", 1);
+
+itensales = [{id_product:1, amount:2, val_product:19.90},
                  {id_product:2, amount:2, val_product:25.90},
-                 {id_product:3, amount:2, val_product:39.90}]
+                 {id_product:3, amount:2, val_product:39.90}];
 
-let sales = {id:1, name:1, disc_sale:2.30}; 
+sales = {id:1, name:1, disc_sale:2.30}; 
 
-var sale:Pg = new Sale(sales.id, sales.name, sales.disc_sale);
+const sale:Pg = new Sale(sales.id, sales.name, sales.disc_sale);
 
 //console.log(sale)
 //console.log(itensales)
